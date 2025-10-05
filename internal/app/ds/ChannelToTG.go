99432dfe9daa70345989b0caf2a5ef914b3b0e47
bifidok1211 <iliaru121105@gamil.com>
2025-10-05 22:01:20 +0300

    update

--- internal/app/ds/ChannelToTG.go
+++ internal/app/ds/ChannelToTG.go
@@ -1,8 +1,9 @@
 package ds
 
 type ChannelToTG struct {
-	TGID        uint  `gorm:"primaryKey;column:tg_id;not null"`      // Внешний ключ к ChannelSearching
-	ChannelID   uint  `gorm:"primaryKey;column:channel_id;not null"` // Внешний ключ к Channels
+	ID          uint  `gorm:"primaryKey;column:id"`
+	TGID        uint  `gorm:"column:tg_id;not null"`      // Внешний ключ к ChannelSearching
+	ChannelID   uint  `gorm:"column:channel_id;not null"` // Внешний ключ к Channels
 	Views       *uint `gorm:"column:views;type:bigint"`
 	RepostLevel *uint `gorm:"column:repost_level;type:bigint"`
 
